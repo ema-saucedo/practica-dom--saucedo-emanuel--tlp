a12010938d5ffb0e697c5161b151f5a8c519fd7d
EmaSaucedo <lopeposo748@gmail.com>
2026-06-25 02:23:20 -0300

    Se añadió render de las cards con los personajes

---
+++ assets/js/app.js
@@ -0,0 +1,61 @@
+addEventListener("DOMContentLoaded", (event) => {
+  const personajes = [
+    {
+      id: 1,
+      nombre: "A-Bomb",
+      imagen:
+        "https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/md/1-a-bomb.jpg",
+    },
+    {
+      id: 2,
+      nombre: "Abe Sapien",
+      imagen:
+        "https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/md/2-abe-sapien.jpg",
+    },
+    {
+      id: 3,
+      nombre: "Abin Sur",
+      imagen:
+        "https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/md/3-abin-sur.jpg",
+    },
+    {
+      id: 4,
+      nombre: "Abomination",
+      imagen:
+        "https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/md/4-abomination.jpg",
+    },
+    {
+      id: 5,
+      nombre: "Abraxas",
+      imagen:
+        "https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/md/5-abraxas.jpg",
+    },
+  ];
+
+  console.log(personajes)
+
+  const personajeElements = document.querySelector('#cardPj')
+  
+  let listaPersonajes = '';
+
+  personajes.forEach (personaje => {
+    listaPersonajes += `
+    <div class="col mt-3">
+            <div class="card" style="width: 18rem">
+              <img src="${personaje.imagen}" class="card-img-top" alt="${personaje.nombre}}" />
+              <div class="card-body">
+                <h5 class="card-title">${personaje.nombre}</h5>
+                <p class="card-text">
+                </p>
+                <a href="#" class="btn btn-primary">Borrar</a>
+              </div>
+            </div>
+          </div>`;
+         
+          personajeElements.innerHTML = listaPersonajes;
+ })
+ 
+
+
+
+});
